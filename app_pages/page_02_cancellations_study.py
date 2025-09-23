5b60956975910d8028d392d4b9df47fa1c33375b
Pete Lowry <peter.m.lowry@gmail.com>
2025-09-23 20:11:20 +0100

    Move get_percentage_cancelled to data_management.py
    - The function is also used on dashboard page 3

--- app_pages/page_02_cancellations_study.py
+++ app_pages/page_02_cancellations_study.py
@@ -8,6 +8,7 @@ from src.data_management import (
     load_room_data,
     load_lead_time_data,
     load_image,
+    get_percentage_cancelled
 )
 from src import texts
 
@@ -972,39 +973,6 @@ def section_cancellation_lead_times(df_lead):
             """
         )
         plot_agent_1_cancellations(df_lead)
-
-
-# Helper functions
-def get_percentage_cancelled(df, feature, target, min_total_bookings=0):
-    """
-    Calculate total bookings and percentage cancellations for each group of a
-    specified feature. Optionally apply a minimum bookings threshold, and
-    return a dataframe summarizing results sorted by cancellation percentage.
-
-    This function assumes `target` is the column `is_canceled` with boolean
-    categories (True = cancelled, False = not cancelled).
-    """
-
-    # Get counts
-    counts = df.groupby([feature, target], observed=False).size().reset_index(name='count')
-    counts = counts.pivot(index=feature, columns=target, values='count').fillna(0)
-    counts.columns.name = None
-
-    # Add total bookings and % cancelled
-    summary = counts.copy()
-    summary['Total Bookings'] = summary.sum(axis=1).astype('int')
-    summary['% Cancelled'] = round(summary[True] / summary['Total Bookings'] * 100,1)
-
-    # Filter for relevant columns
-    summary = summary[['Total Bookings', '% Cancelled']]
-
-    # Filter out countries with total bookings below a threshold
-    condition = summary['Total Bookings'] >= min_total_bookings
-    summary = summary[condition]
-
-    # Sort and display
-    summary.sort_values('% Cancelled', ascending=False, inplace=True)
-    return summary
 
 
 # Plotting functions
