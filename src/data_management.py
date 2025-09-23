4c126fba2a950eb87937a2f05ba5cd908274ea3d
Pete Lowry <peter.m.lowry@gmail.com>
2025-09-23 19:22:22 +0100

    Add load_percentage_cancellations_by_country_data and move get_percentage_cancelled
    - Added load_percentage_cancellations_by_country_data function
    - Moved get_percentage_cancelled out of the dashboard app page so it can also be used on dashboard page 3

--- src/data_management.py
+++ src/data_management.py
@@ -47,7 +47,51 @@ def load_lead_time_data(version=VERSION):
     return df
 
 
+@st.cache_data
+def load_percentage_cancellations_by_country_data(version=VERSION):
+    df = pd.read_csv(
+        (
+            f"outputs/{version}/datasets/cleaned/"
+            "percentage_cancellations_by_country_deduplicated.csv"
+        )
+    )
+    return df
+
+
 # Functions for loading images
 def load_image(filename, version=VERSION):
     filepath = f"outputs/{version}/images/{filename}"
     st.image(filepath)
+
+
+# Helper functions
+def get_percentage_cancelled(df, feature, target, min_total_bookings=0):
+    """
+    Calculate total bookings and percentage cancellations for each group of a
+    specified feature. Optionally apply a minimum bookings threshold, and
+    return a dataframe summarizing results sorted by cancellation percentage.
+
+    This function assumes `target` is the column `is_canceled` with boolean
+    categories (True = cancelled, False = not cancelled).
+    """
+
+    # Get counts
+    counts = df.groupby([feature, target], observed=False).size().reset_index(name='count')
+    counts = counts.pivot(index=feature, columns=target, values='count').fillna(0)
+    counts.columns.name = None
+
+    # Add total bookings and % cancelled
+    summary = counts.copy()
+    summary['Total Bookings'] = summary.sum(axis=1).astype('int')
+    summary['% Cancelled'] = round(summary[True] / summary['Total Bookings'] * 100,1)
+
+    # Filter for relevant columns
+    summary = summary[['Total Bookings', '% Cancelled']]
+
+    # Filter out countries with total bookings below a threshold
+    condition = summary['Total Bookings'] >= min_total_bookings
+    summary = summary[condition]
+
+    # Sort and display
+    summary.sort_values('% Cancelled', ascending=False, inplace=True)
+    return summary
